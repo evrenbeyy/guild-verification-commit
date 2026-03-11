3a35f65d69a42c5c789e0b9655d507a03d27b81d
evrenbeyy <166454787+evrenbeyy@users.noreply.github.com>
2026-03-11 13:23:48 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T10:23:46.194Z
-Commit: Add multi-language support
+Timestamp: 2026-03-11T10:23:48.616Z
+Commit: Resolve null pointer exception in user service
