886f99e286583fd84c3a2913448ae691f5196cd5
evrenbeyy <166454787+evrenbeyy@users.noreply.github.com>
2026-03-11 13:23:53 +0300

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T10:23:42.506Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-11T10:23:53.511Z
+Commit: Correct typo in configuration file
