3b06ee359e2d60a2f3d4924cf0bb4bfeae14598d
evrenbeyy <166454787+evrenbeyy@users.noreply.github.com>
2026-03-11 14:50:33 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:50:32.131Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-11T11:50:34.269Z
+Commit: Patch API endpoint error handling
