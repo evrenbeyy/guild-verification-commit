cb6fa4303c52fbd90f6a5734b38217407b304746
evrenbeyy <166454787+evrenbeyy@users.noreply.github.com>
2026-03-11 14:50:34 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:50:34.269Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-11T11:50:35.333Z
+Commit: Patch security vulnerability in authentication
